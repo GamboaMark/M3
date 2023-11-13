7b68ebf5de3e4e7a842e7b1b0a886d2dc3e5772f
Mark <gamboamarkgerard@gmail.com>
2023-11-13 09:11:46 +0800

    first commit

---
+++ Gamboa_3_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Day        Time            Course Code     Section     Room\n";
+    cout << "Teusday    16:00-18:40     CCS0003         TW04        Online\n";
+    cout << "Wednesday  16:00-18:50     CCS0003L        TW04        F1203\n";
+    return 0;
+}
